86c58f1665f5f8b93a90119be54e1a67204c25b5
WertiPL <wiktor.rostkowski@hotmail.com>
2022-06-18 09:22:50 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs23141nbp/JazS23141NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs23141nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23141NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS23141NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs23141nbp/JazS23141NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs23141nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23141NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
